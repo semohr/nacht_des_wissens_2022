1050a71c86bfcc3d7e354b06d5218e9480c4af68
Sebastian Mohr <sebastian@mohrenclan.de>
2022-06-23 14:55:35 +0200

    Added animation to number history

--- components/experiment/NumberHistory.tsx
+++ components/experiment/NumberHistory.tsx
@@ -1,35 +1,36 @@
 import useSocket from "lib/useSocket";
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 
-
-export default function NumberHistory({number=undefined}) {
+export default function NumberHistory({ number = undefined }) {
     const [history, setHistory] = useState([]);
 
+    useEffect(() => {
+        if (number) {
+            addToHistory(number);
+        }
+    }, [number]);
 
-    useEffect(()=>{
-        if (number){
-            addToHistory(<div className="">{number}</div>);
-    }},[number]);
+    const addToHistory = (number) => {
+        // add new_num tag to the element which is removed after 1 second
+        let element = <div key={number + "-" + Math.random()}>{number}</div>;
 
-    const addToHistory = (element)=>{
         //Check length and add or remove
-        const max = 5;
-        if (history.length <= max){
+        const max = 10;
+        if (history.length <= max) {
             setHistory([...history, element]);
         } else {
             setHistory([...history.slice(1, history.length), element]);
         }
-    }
-
-    // addToHistory(<div>1</div>);
-    // addToHistory(<div>2</div>);
-    // addToHistory(<div>3</div>);
-    // addToHistory(<div>4</div>);
-    // addToHistory(<div>num</div>);
+    };
 
     return (
-        <div className="d-flex flex-row numHistory" id="numHistory">
-            {history}
+        <div className="d-flex w-50">
+            <div
+                className="ml-2 d-flex flex-row numHistory w-100"
+                id="numHistory"
+            >
+                {history}
+            </div>
         </div>
-    )
+    );
 }
--- components/experiment/Prompt.tsx
+++ components/experiment/Prompt.tsx
@@ -1,9 +1,9 @@
 import useSocket from "lib/useSocket";
 import useTranslation from "next-translate/useTranslation";
 import { useEffect, useState } from "react";
+import NumberHistory from "./NumberHistory";
 
 export default function Prompt({ expID, exp_is_running = true }) {
-
     const [num, setNum] = useState(null);
     const [color, setColor] = useState("black");
     const socket = useSocket();
@@ -11,7 +11,6 @@ export default function Prompt({ expID, exp_is_running = true }) {
     //Translation
     const { t } = useTranslation("common");
     const prompt = t("prompt");
-
 
     useEffect(() => {
         if (socket) {
@@ -21,25 +20,24 @@ export default function Prompt({ expID, exp_is_running = true }) {
                     setColor("black");
                 }, 10);
                 setNum(random_number);
-            })
+            });
         }
     }, [socket]);
 
     // only show the readout prompt if the experiment is running
     if (!exp_is_running) {
-        return (<></>);
+        return <></>;
     }
 
     return (
         <>
             <div className="prompt">
-                <div className="text">
-                    {prompt}
-                </div>
-                <div className="number" style={{ "color": color }}>
+                <div className="text">{prompt}</div>
+                <div className="number" style={{ color: color }}>
                     {!num ? "[?]" : num}
                 </div>
             </div>
+            <NumberHistory number={num}></NumberHistory>
         </>
     );
 }
